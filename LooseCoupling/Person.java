da147fa38e4b46298cf5ddfc31d4e3cbbe7d8db8
Keerthivasan28062002 <149353012+Keerthivasan28062002@users.noreply.github.com>
2023-11-09 20:33:20 +0530

    Create Person.java

---
+++ LooseCoupling/Person.java
@@ -0,0 +1,14 @@
+package com.loosecoupling;
+
+public class Person {
+
+	public static void main(String[] args) {
+		Transport b=new Bus();
+		Transport t=new Train();
+		Itravel t1=new Travel(t);
+    //Itravel t1=new Travel(b);
+		t1.show();
+
+	}
+
+}
