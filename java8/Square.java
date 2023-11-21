d29ee49827c78bb30e98ce891ca46ab6061becb3
Keerthivasan28062002 <149353012+Keerthivasan28062002@users.noreply.github.com>
2023-11-21 18:45:25 +0530

    Update Square.java

--- java8/Square.java
+++ java8/Square.java
@@ -11,7 +11,7 @@ public interface Square {
 		System.out.print("Cube of Given Number:");
 		System.out.println(a*a*a);
 	}
-	private static void squareRoot(int a) {
+	public static void squareRoot(int a) {
 		System.out.print("Square Root of Given Number:");
 		System.out.println(Math.sqrt(a));
 	}
